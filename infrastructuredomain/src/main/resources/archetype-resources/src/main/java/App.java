55da0858a7f43dd66eb3595a74b69f4c2b74363d
Carlos Herrera <cherrera@preverisk.com>
2024-03-09 13:59:33 +0100

    infrastructuredoamin created

---
+++ infrastructuredomain/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.paymentchain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
